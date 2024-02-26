2152804e32b0f70f622da56489783eb86ea53e92
ruthsopoyian <ruthsopoyian@gmail.com>
2024-02-26 16:29:02 +0300

    admin files

--- afternoon/class/models.py
+++ afternoon/class/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    location =models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    number = models.IntegerField()
+    location = models.CharField(max_length=40)
+    subject = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    cost = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class Assignment(models.Model):
+    book = models.CharField(max_length=50)
+    pages = models.IntegerField()
+    duration = models.IntegerField()
